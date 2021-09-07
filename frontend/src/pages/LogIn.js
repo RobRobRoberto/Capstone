7107f063ec5254017ea2bd4a5d40dbf7ada293a0
RobRobRoberto <robert.boellert@gmail.com>
2021-09-07 20:54:11 +0200

    added DB config

--- frontend/src/pages/LogIn.js
+++ frontend/src/pages/LogIn.js
@@ -1,12 +1,28 @@
 import styled from 'styled-components/macro'
+import { useState } from 'react'
+
+const initialState = {
+  username: '',
+  password: '',
+}
 
 export default function LogIn() {
+  const [credentials, setCredentials] = new useState(initialState)
+
+  function handleChange(event) {
+    setCredentials({ ...credentials, [event.target.name]: event.target.value })
+
+    console.log(credentials)
+  }
+
+  // Innerhalb des input Tags ist das Attribut "value" dafür da den 'initial value' festzulegen.
+  // Das heißt er ist in meinem Bsp. leer.
   return (
     <Wrapper>
       <h2>Bitte logge Dich ein</h2>
 
-      <input type="text" name="username" />
-      <input type="text" name="password" />
+      <input onChange={handleChange} value={credentials.username} />
+      <input onChange={handleChange} value={credentials.password} />
       <button>anmelden</button>
     </Wrapper>
   )
